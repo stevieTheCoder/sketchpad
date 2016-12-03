63fce417c3e5858b497a905161ce54be92f67909
Steven Rogers <steven.rogers1990@gmail.com>
2016-12-03 13:52:51 +0000

    grid created

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    for(var x = 0; x < 16; x++) {
+        for(var y = 0; y < 16; y++) {
+            var unit = $("<div class='unit'></div>");
+            unit.appendTo('#container');
+        }
+    }
+});
+
