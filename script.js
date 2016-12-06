22ef2955a4b57132fc06fde3c4c849fe87cbf018
Steven Rogers <steven.rogers1990@gmail.com>
2016-12-06 20:10:25 +0000

    finished

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
 $(document).ready(function () {
 
-    var gridSize = 16;
-    var gridWidthHeight = 600 / gridSize;
-
-    var newGridSize = 16;
-    var newGridWidth = 600 / newGridSize;
-
-    //creates a grid of 16 and appends to container div
     var createGrid = function (tileNumber, tileSize) {
         for (var x = 0; x < tileNumber; x++) {
             for (var y = 0; y < tileNumber; y++) {
                 var unit = $("<div class='unit'></div>");
                 unit.appendTo('#container');
-                $('.unit').css('height', tileSize).css('width', tileSize);
+                $('.unit').css('height', tileSize + "%").css('width', tileSize + "%");
             }
         }
     }
 
-    createGrid(gridSize, gridWidthHeight);
+    function changeBlack() {
+        $('.unit').hover(function () {
+            $(this).addClass('hoverBlack');
+        });
+    }
 
-    //mouse over effect to change color
-    $('.unit').hover(function () {
-        $(this).addClass('hover');
-    });
+    function changeRandom() {
+        $('.unit').hover(function () {
+            $(this).css('background-color', getRandomColor());
+        });
+    }
 
-    //reset grid
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
     $('#reset').click(function () {
         $('.unit').remove();
-        newGridSize = prompt('Enter grid size:');
-        console.log(newGridSize);
-        console.log(newGridWidth);
+        var tileNumber = prompt('Enter grid size:');
+        var tileSize = (((600 / tileNumber) / 600) * 100);
+        createGrid(tileNumber, tileSize);
+        changeBlack();
     });
+
+    $('#randomColor').click(function () {
+        changeRandom();
+    });
+
+    createGrid(16, 6.25);
+    changeBlack();
 
 });
